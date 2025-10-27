3c942e1c95d00efd8c439bb329cd3a54acc32a95
Ryan Oliveira <135620793+oryanend@users.noreply.github.com>
2025-10-27 20:40:54 +0000

    Prettier Configuration

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-  return <h1>Você é... amigo!</h1>
+function Home() {
+  return <h1>Você é... amigo!</h1>;
+}
+
+function ZNote() {
+  return console.log("Salvar");
+}
+
+function YNote() {
+  return console.log("Test - Saving");
 }
 
 export default Home;
