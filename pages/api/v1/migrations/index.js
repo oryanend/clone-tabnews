ab38b91baa0d88912b31f7e2d050f093e400eb64
Ryan Oliveira <135620793+oryanend@users.noreply.github.com>
2025-11-10 14:13:35 +0000

    fix /migrations endpoint corrections bugs

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,16 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database";
+import { error } from "node:console";
 
 export default async function migrations(request, response) {
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method ${request.method} Not Allowed`,
+    });
+  }
+
   if (request.method == "GET") {
     await pendingMigrations(true);
   }
