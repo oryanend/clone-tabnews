b597851a5cc2979ae5b9b3c90b4dea60998cc4fc
Ryan Oliveira <135620793+oryanend@users.noreply.github.com>
2025-11-29 23:02:36 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,27 +6,35 @@ beforeAll(async () => {
   await database.query("DROP schema public CASCADE; CREATE schema public;");
 });
 
-test("POST should Return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
+
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+      test("For the second time", async () => {
+        const responseSecond = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(responseSecond.status).toBe(200);
+
+        const responseSecondBody = await responseSecond.json();
+        expect(Array.isArray(responseSecondBody)).toBe(true);
+        expect(responseSecondBody.length).toBe(0);
+      });
+    });
   });
-  expect(response.status).toBe(201);
-
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  // SECOND CALL
-
-  const responseSecond = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    },
-  );
-  expect(responseSecond.status).toBe(200);
-
-  const responseSecondBody = await responseSecond.json();
-  expect(Array.isArray(responseSecondBody)).toBe(true);
-  expect(responseSecondBody.length).toBe(0);
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,22 +4,30 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET should Return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+      const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.current_connections).toEqual(1);
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.current_connections).toEqual(1);
+    });
+  });
 });
 
-test("GET should Return 404 when version doesn't exist", async () => {
-  const response = await fetch("http://localhost:3000/api/v2/status");
-  expect(response.status).toBe(404);
+describe("GET /api/v2/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving a not found page", async () => {
+      const response = await fetch("http://localhost:3000/api/v2/status");
+      expect(response.status).toBe(404);
+    });
+  });
 });
