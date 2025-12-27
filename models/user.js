d5a1363df87003d4b1a09eefcddba89aabe93e37
Ryan Oliveira <135620793+oryanend@users.noreply.github.com>
2025-12-27 20:40:03 +0000

    feat: create `user.findByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,35 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
+
+async function findByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+        SELECT  
+          *
+        FROM 
+          users 
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+        ;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O nome de usuario não foi encontrado.",
+        action: "Verifique o nome de usuario e tente novamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validateUsername(userInputValues.username);
@@ -70,6 +100,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findByUsername,
 };
 
 export default user;
---
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -0,0 +1,96 @@
+import { version as uuidVersion } from "uuid";
+import orchestrator from "tests/orchestrator.js";
+import database from "infra/database.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("GET /api/v1/users/[username]", () => {
+  describe("Anonymous user", () => {
+    test("With exact case match", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "MesmoCase",
+          email: "mesmocase@example.com",
+          password: "somepassword123",
+        }),
+      });
+      expect(response.status).toBe(201);
+
+      const getResponse = await fetch(
+        `http://localhost:3000/api/v1/users/MesmoCase`,
+      );
+      expect(getResponse.status).toBe(200);
+
+      const responseBody = await getResponse.json();
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "MesmoCase",
+        email: "mesmocase@example.com",
+        password: "somepassword123",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+
+    test("With exact case miss match", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "CaseDiff",
+          email: "casediff@example.com",
+          password: "somepassword123",
+        }),
+      });
+      expect(response.status).toBe(201);
+
+      const getResponse = await fetch(
+        `http://localhost:3000/api/v1/users/casediff`,
+      );
+      expect(getResponse.status).toBe(200);
+
+      const responseBody = await getResponse.json();
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "CaseDiff",
+        email: "casediff@example.com",
+        password: "somepassword123",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+
+    test("With non-existent username", async () => {
+      const getResponse = await fetch(
+        `http://localhost:3000/api/v1/users/NonExistentUser`,
+      );
+      expect(getResponse.status).toBe(404);
+
+      const responseBody = await getResponse.json();
+      expect(responseBody).toEqual({
+        name: "NotFoundError",
+        message: "O nome de usuario não foi encontrado.",
+        action: "Verifique o nome de usuario e tente novamente.",
+        statusCode: 404,
+      });
+    });
+  });
+});
