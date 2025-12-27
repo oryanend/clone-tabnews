56c528d86b897d21f9f780d9f43471391a56633a
Ryan Oliveira <135620793+oryanend@users.noreply.github.com>
2025-12-27 20:38:31 +0000

    feat: create `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -76,3 +76,23 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Não foi possivel encontrar o recurso solicitado.", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action = action || "Verifique se os parâmetros estão corretos.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      statusCode: this.statusCode,
+    };
+  }
+}
