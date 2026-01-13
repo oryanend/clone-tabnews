034f3735c9a276afaeb94b984e35dc22f000b3e6
Ryan Oliveira <135620793+oryanend@users.noreply.github.com>
2026-01-13 01:34:58 +0000

    feat: implement `/api/v1/user` endpoint with session renew

---
+++ tests/integration/api/v1/user/get.test.js
@@ -0,0 +1,183 @@
+import { version as uuidVersion } from "uuid";
+import orchestrator from "tests/orchestrator.js";
+import database from "infra/database.js";
+import session from "models/session";
+import setCookieParser from "set-cookie-parser";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("GET /api/v1/user", () => {
+  describe("Default user", () => {
+    test("With valid session", async () => {
+      const createdUser = await orchestrator.createUser({
+        username: "UserWithValidSession",
+      });
+
+      const sessionObj = await orchestrator.createSession(createdUser.id);
+
+      const response = await fetch("http://localhost:3000/api/v1/user", {
+        headers: {
+          Cookie: `session_id=${sessionObj.token}`,
+        },
+      });
+      expect(response.status).toBe(200);
+
+      const cacheControlHeader = response.headers.get("Cache-Control");
+      expect(cacheControlHeader).toBe(
+        "no-store, no-cache, max-age=0, must-revalidate",
+      );
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        created_at: createdUser.created_at.toISOString(),
+        email: createdUser.email,
+        id: createdUser.id,
+        password: createdUser.password,
+        updated_at: createdUser.updated_at.toISOString(),
+        username: createdUser.username,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      // Session renewal check
+      const renewedSessionObject = await session.findByValidToken(
+        sessionObj.token,
+      );
+
+      expect(renewedSessionObject.expires_at > sessionObj.expires_at).toBe(
+        true,
+      );
+      expect(renewedSessionObject.updated_at > sessionObj.updated_at).toBe(
+        true,
+      );
+
+      // Set-cookies check
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: sessionObj.token,
+        maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+        path: "/",
+        httpOnly: true,
+      });
+    });
+
+    test("With half session expiration time", async () => {
+      jest.useFakeTimers({
+        now: new Date(Date.now() - session.EXPIRATION_IN_MILLISECONDS / 2),
+      });
+
+      const createdUser = await orchestrator.createUser({
+        username: "UserWithHalfSession",
+      });
+
+      const sessionObj = await orchestrator.createSession(createdUser.id);
+
+      jest.useRealTimers(); // necessário se houver outros testes depois deste para não afetar o tempo global
+      const response = await fetch("http://localhost:3000/api/v1/user", {
+        headers: {
+          Cookie: `session_id=${sessionObj.token}`,
+        },
+      });
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        created_at: createdUser.created_at.toISOString(),
+        email: createdUser.email,
+        id: createdUser.id,
+        password: createdUser.password,
+        updated_at: createdUser.updated_at.toISOString(),
+        username: createdUser.username,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      // Session renewal check
+      const renewedSessionObject = await session.findByValidToken(
+        sessionObj.token,
+      );
+
+      expect(renewedSessionObject.expires_at > sessionObj.expires_at).toBe(
+        true,
+      );
+      expect(renewedSessionObject.updated_at > sessionObj.updated_at).toBe(
+        true,
+      );
+
+      // Set-cookies check
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: sessionObj.token,
+        maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+        path: "/",
+        httpOnly: true,
+      });
+    });
+
+    test("With nonexistent session", async () => {
+      const nonexistentToken =
+        "2c98d6edb2614cc752666ddf0b267e933a9cd84c929e8441f7788565ac594943ddc56b7f85f20d98c6b08df9f7ce446b";
+
+      const response = await fetch("http://localhost:3000/api/v1/user", {
+        headers: {
+          Cookie: `session_id=${nonexistentToken}`,
+        },
+      });
+
+      expect(response.status).toBe(401);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Usuário não possui sessão ativa.",
+        action: "Verifique se este usuário está logado e tente novamente.",
+        statusCode: 401,
+      });
+    });
+
+    test("With expired session", async () => {
+      jest.useFakeTimers({
+        now: new Date(Date.now() - session.EXPIRATION_IN_MILLISECONDS),
+      });
+
+      const createdUser = await orchestrator.createUser({
+        username: "UserWithExpiredSession",
+      });
+
+      const sessionObj = await orchestrator.createSession(createdUser.id);
+
+      jest.useRealTimers(); // necessário se houver outros testes depois deste para não afetar o tempo global
+
+      const response = await fetch("http://localhost:3000/api/v1/user", {
+        headers: {
+          Cookie: `session_id=${sessionObj.token}`,
+        },
+      });
+      expect(response.status).toBe(401);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Usuário não possui sessão ativa.",
+        action: "Verifique se este usuário está logado e tente novamente.",
+        statusCode: 401,
+      });
+    });
+  });
+});
