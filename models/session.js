0e31f91d49acdf7d62d3756340217113f2fb7269
Ryan Oliveira <135620793+oryanend@users.noreply.github.com>
2026-01-15 02:07:52 +0000

    feat: implement session delete

--- models/session.js
+++ models/session.js
@@ -88,10 +88,30 @@ async function renew(sessionId) {
   }
 }
 
+async function expireById(sessionId) {
+  const results = await database.query({
+    text: `
+    UPDATE
+      sessions
+    SET
+      expires_at = expires_at - interval '1 year',
+      updated_at = NOW()
+    WHERE
+      id = $1
+    RETURNING
+      *
+    ;
+    `,
+    values: [sessionId],
+  });
+  return results.rows[0];
+}
+
 const session = {
   renew,
   create,
   findByValidToken,
+  expireById,
   EXPIRATION_IN_MILLISECONDS,
 };
 
